ea650e1904394a63e956600f98e73c0cae70caee
bryce <brycekbargar@gmail.com>
2021-08-20 13:06:32 -0500

    Add the todomvc page with alpine

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,16 @@
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addShortcode("rendersection", function (name) {
+    if (!this.page.sections) return "";
+
+    return this.page.sections[name] || "";
+  });
+  eleventyConfig.addPairedShortcode("section", function (content, name) {
+    this.page.sections = this.page.sections || {};
+    this.page.sections[name] = content;
+
+    return "";
+  });
+
   return {
     dir: {
       input: "site",
